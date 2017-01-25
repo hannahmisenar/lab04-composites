74b72eb086e4522aa44761790566c250dc1d08f8
hmisenar <hannahamisenar@gmail.com>
2017-01-25 11:51:05 -0800

    Submission

--- composite-dom.ts
+++ composite-dom.ts
@@ -2,3 +2,19 @@ interface IDomElement {
   print();
 }
 
+class DomElement {
+ html = new TextNode('html');
+ 
+}
+
+class TextNode{
+  private type;  
+  constructor(type: string) {
+    this.type = type;
+  }
+toString() {
+  print(this.type);
+}
+}
+
+}
